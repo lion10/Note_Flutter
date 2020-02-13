d91ab90b92d231e6b22525ef5bcf3a9b0dda7278
omar suliman abu sabha <ssomar2028@gmail.com>
2020-02-13 11:07:26 +0200

    add note class to get just one item with title content , make the code more clear than before with factory methode in each note , noteforlisting class

--- lib/Service/notes_service.dart
+++ lib/Service/notes_service.dart
@@ -2,6 +2,7 @@
 import 'dart:convert';
 
 import 'package:note_app/models/api_response.dart';
+import 'package:note_app/models/note.dart';
 import 'package:note_app/models/note_for_listing.dart';
 import 'package:http/http.dart' as http;
 
@@ -10,6 +11,8 @@ class NotesService{
   static const API  ='http://api.notes.programmingaddict.com';
   static const Headers = {"apiKey": "08d7aeca-086f-1f17-4d05-95beda7a1e45"};
 
+
+  // for list of items
   Future<ApiResponse<List<NoteForListing>>> getNoteList() async{
     return http.get(API+'/notes',headers: Headers).
     then((data){
@@ -17,19 +20,30 @@ class NotesService{
               final jsonData  =json.decode(data.body);
               final notes = <NoteForListing>[];
                 for(var item in jsonData){
-                  final note = NoteForListing(
-                      noteID: item['noteID'] ,
-                      noteTitle:item['noteTitle'],
-                      createDateTime:DateTime.parse(item['createDateTime']) ,
-                      latestEditedDateTime: item['latestEditDateTime'] != null ? DateTime.parse(item['latestEditDateTime']) : null);
-
-                  notes.add(note);
+                  notes.add(NoteForListing.fromJson(item));
                 }
                 return ApiResponse<List<NoteForListing>>(data: notes);
             }
           return ApiResponse<List<NoteForListing>>(error: true , errorMessage: "An Error Is Occured");
     }).catchError((_){
       return ApiResponse<List<NoteForListing>>(error: true , errorMessage: "An Error Is Occured");
+    });
+  }
+
+  // for one item only
+  Future<ApiResponse<Note>> getNote(String noteID) async {
+    return http.get(API + '/notes/' + noteID, headers: Headers).
+    then((data) {
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+
+        return ApiResponse<Note>(data: Note.fromJson(jsonData));
+      }
+      return ApiResponse<Note>(
+          error: true, errorMessage: "An Error Is Occured");
+    }).catchError((_) {
+      return ApiResponse<Note>(
+          error: true, errorMessage: "An Error Is Occured");
     });
 
   }
--- lib/models/note_for_listing.dart
+++ lib/models/note_for_listing.dart
@@ -10,4 +10,15 @@ class NoteForListing{
     this.noteTitle,
     this.createDateTime,
     this.latestEditedDateTime});
+
+  factory NoteForListing.fromJson(Map<String , dynamic> item) {
+
+    return NoteForListing(
+            noteID: item['noteID'] ,
+            noteTitle:item['noteTitle'],
+            createDateTime:DateTime.parse(item['createDateTime']) ,
+            latestEditedDateTime: item['latestEditDateTime'] != null ? DateTime.parse(item['latestEditDateTime']) : null
+    );
+
+  }
 }
